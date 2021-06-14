f6710b88448b1f7605156c6f422acfef6e6463cb
kylec <KYLECOHEN14@GMAIL.COM>
2021-06-14 17:28:16 -0400

    trying to branch

---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const storeArray = [Seattle, Tokyo, Dubai, Paris, Lima];
+
+// function render
+
+
+const Seattle = {
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3
+};
+const Tokyo = {
+  
+}
+const Dubai = {
+
+}
+const Paris = {
+
+}
+const Lima = {
+
+}
+// ****create global array for hours
+// 
+
+// create a function to take in 2 numbers (min and max)
+// ******that generates a random number between min and max
+// 
+// each object will need a list (array), the random number generator will be used to build list of random numbers for every hour on every store (one store today)
+// once that list is done, go through creating DOM elements, giving it values, and appending it to the DOM
+// grab something to append to (id)
+// create new element
+// give that new element some content (values)
+// append that element to the thing grabbed
+
+// give LI values the random number (#of customer) * by avg cookie sale at that store, each LI is the hour of the day
+// 
